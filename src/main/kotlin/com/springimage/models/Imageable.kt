fe0b02e5b0d9240d384e8f5822d2ae47e3554182
Ryan Batchelder <ryan.batchelder@libertymutual.com>
2016-08-19 11:14:27 -0400

    Removing unused interface

--- src/main/kotlin/com/springimage/models/Imageable.kt
+++
@@ -1,8 +0,0 @@
-package com.springimage.models
-
-interface Imageable {
-    fun getImage(): ImageDataField;
-    fun getImage(key: String): ImageDataField;
-    fun setImage(key: String, imageData: ImageDataField);
-    fun hasImage(): Boolean;
-}
